2dc6194f48b58693046f9ffd2a8eadb00dcc9b3d
huongmt3 <huongmtgcd201620@fpt.edu.vn>
2023-07-17 09:37:55 +0700

    Add testStack

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,36 +1,50 @@
+import implementations.Stack;
+
 import java.util.ArrayList;
 import java.util.Iterator;
 
 
 public class Main {
     public static void main(String[] args) {
-        public static void testArrayList {
-            /**Display custom ArrayList*/
-            implementations.ArrayList.insert();
-            System.out.println("\nelement at index in custom ArrayList > " + 1 + " = " + List < E >.get(1));
+        public static void testArrayList{
+            ArrayList<Integer> list = new ArrayList<>();
+            list.add(1);
+            list.add(2);
+            list.add(3);
+            list.add(4);
+            list.add(5);
+            System.out.println("Original List: " + list);
+            System.out.println("Size of List: " + list.size());
+            System.out.println("Element at index 2: " + list.get(2));
+            System.out.println("Removing element at index 2: " + list.remove(2));
+            System.out.println("List after removing element at index 2: " + list);
+            System.out.println("List after adding element 6 at index 2: " + list);
+        }
+        public static void testStack{
+        Stack<Integer> stack = new Stack<>();
 
-            /**Remove element from custom ArrayList*/
-            System.out.println("element removed from index " + 1 + " = "
-                    + List < E >.remove(1));
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
 
-            /**Again display custom ArrayList*/
-            System.out
-                    .println("\nlet's display custom ArrayList again after removal at index 1");
+        System.out.println(stack.peek()); // 3
 
-            implementations.ArrayList.display();
+        int element = stack.pop();
+        System.out.println(element); // 3
 
-            ArrayList<E>  =new ArrayList<E>();
-            implementations.ArrayList.add(1);
-            List < E >.add(2);
-            List < E >.add(3);
-            System.out.println(List < E >.get(0)); // 1
-            System.out.println(List < E >.get(1)); // 2
-            System.out.println(List < E >.get(2)); // 3
-            List < E >.set(1, 4);
-            System.out.println(List < E >.get(1)); // 4
-            List < E >.remove(2);
-            System.out.println(List < E >.get(1)); // 4
-            System.out.println(List < E >.size()); // 2
-                }
+        System.out.println(stack.size()); // 2
+
+        boolean isEmpty = stack.isEmpty();
+        System.out.println(isEmpty); // false
+
+        Iterator<Integer> iterator = stack.iterator();
+        while (iterator.hasNext()) {
+            int value = iterator.next();
+            System.out.println(value);
             }
         }
+        public static void testQueue{
+
+        }
+    }
+}
