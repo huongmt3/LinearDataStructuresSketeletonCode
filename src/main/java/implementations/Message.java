1d195a7551a731b2e436585c9f173602b0bde9e0
huongmt3 <huongmtgcd201620@fpt.edu.vn>
2023-08-17 06:28:00 +0700

    add JUnit 5.8.1 done

--- src/main/java/implementations/Message.java
+++ src/main/java/implementations/Message.java
@@ -92,5 +92,28 @@ public class Message {
      * | Exceptions handled in inputting message |
      * ==========================================
      */
+/**try {
+        StringBuilder messCollection = new StringBuilder();
+        String inputtedMess = inputMess();
+        while (!inputtedMess.equalsIgnoreCase("end")){
+        }
+        System.out.println();
+        long start = System.currentTimeMillis();
+        String[] messData = messCollection.toString().split("\n");
+        Queue<Message> transferredMess = transferMess(messData);
+        Stack<Message> processedMess = processMess(transferredMess);
+        System.out.println("Message in stack to be processed: ");
+        for (Message mess : processedMess)
+            System.out.println("\u001B[35m" + mess.destMess + "\u001B[0m");
+        long end = System.currentTimeMillis();
+        long time = end - start;
+        System.out.println("Total execution time: \u001B[35m" + time + "\u991B[0m ms.");
+    }
+        catch (IllegalArgumentException error){
+        System.out.println("\u001B[31mMessage is empty.\nProcess terminated.\u001B[0m");
+    }
+        catch (IllegalStateException error){
+        System.out.println("\u001B[31mMessage exceeds the limit of 250 characters.\nProcess terminated.\u001B[0m");
+    }*/
 
 }
